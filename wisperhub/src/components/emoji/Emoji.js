0a9eff4ab9134c294216cf50367daba88f993e09
joris budin <jorisbudin@gmail.com>
2024-05-05 17:12:32 +0200

    create a button emoji who display a emoji panel whose when we click on a emote, the emote is add in the input message

---
+++ wisperhub/src/components/emoji/Emoji.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import styles from "./Emoji-style.css";
+
+export default function Emoji({ messageText, setMessageText }) {
+  const emoji = [
+    "🙂​",
+    "😂",
+    "😝",
+    "😁",
+    "😱",
+    "👉",
+    "🙌",
+    "🍻",
+    "🔥",
+    "🌈",
+    "☀",
+    "🎈",
+    "🌹",
+    "💄",
+    "🎀",
+    "⚽",
+    "🎾",
+    "🏁",
+    "😡",
+    "👿",
+    "🐻",
+    "🐶",
+    "🐬",
+    "🐟",
+    "🍀",
+    "👀",
+    "🚗",
+    "🍎",
+    "💝",
+    "💙",
+    "👌",
+    "❤",
+    "😍",
+    "😉",
+    "😓",
+    "😳",
+    "💪",
+    "💩",
+    "🍸",
+    "🔑",
+    "💖",
+    "🌟",
+    "🎉",
+    "🌺",
+    "🎶",
+    "👠",
+    "🏈",
+    "⚾",
+    "🏆",
+    "👽",
+    "💀",
+    "🐵",
+    "🐮",
+    "🐩",
+    "🐎",
+    "💣",
+    "👃",
+    "👂",
+    "🍓",
+    "💘",
+    "💜",
+    "👊",
+    "💋",
+    "😘",
+    "😜",
+    "😵",
+    "🙏",
+    "👋",
+    "🚽",
+    "💃",
+    "💎",
+    "🚀",
+    "🌙",
+    "🎁",
+    "⛄",
+    "🌊",
+    "⛵",
+    "🏀",
+    "🎱",
+    "💰",
+    "👶",
+    "👸",
+    "🐰",
+    "🐷",
+    "🐍",
+    "🐫",
+    "🔫",
+    "👄",
+    "🚲",
+    "🍉",
+    "💛",
+    "💚",
+  ];
+  const [toogleEmoji, setToogleEmoji] = useState(false);
+
+  const handleEmoji = () => {
+    setToogleEmoji(!toogleEmoji);
+  };
+
+  function handleEmote(emote) {
+    setMessageText(`${messageText}${emote}`);
+  }
+
+  console.log(toogleEmoji);
+  return (
+    <div>
+      <button type="button" onClick={handleEmoji}>
+        🙂​
+      </button>
+      <div className={toogleEmoji === true ? "containerEmoji" : "none"}>
+        {emoji.map((emote) => (
+          <button
+            className="emote"
+            type="button"
+            onClick={() => handleEmote(emote)}
+            key={emote}
+          >
+            {emote}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
