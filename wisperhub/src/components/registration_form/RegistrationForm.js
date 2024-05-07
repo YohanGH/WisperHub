519de4b24f80f95b432eb6348e47884a4ec447f1
YohanGH <YohanGH@proton.me>
2024-05-07 14:59:33 +0200

    Modification css and add password and confirmation password

--- wisperhub/src/components/registration_form/RegistrationForm.js
+++ wisperhub/src/components/registration_form/RegistrationForm.js
@@ -12,59 +12,92 @@ export default function RegistrationForm() {
 	console.log(errors);
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input
-				className="form-input"
-				type="text"
-				placeholder="First name"
-				{...register("First name", { required: true, maxLength: 80 })}
-			/>
-			<input
-				className="form-input"
-				type="text"
-				placeholder="Last name"
-				{...register("Last name", { required: true, maxLength: 100 })}
-			/>
-			<input
-				className="form-input"
-				type="text"
-				placeholder="Email"
-				{...register("Email", {
-					required: true,
-					pattern: /^\S+@\S+$/i,
-				})}
-			/>
-			<input
-				className="form-input"
-				type="tel"
-				placeholder="Mobile number"
-				{...register("Mobile number", {
-					required: true,
-					minLength: 6,
-					maxLength: 12,
-				})}
-			/>
-			<select {...register("Title", { required: true })}>
-				<option value="Mr">Mr</option>
-				<option value="Mrs">Mrs</option>
-				<option value="Miss">Miss</option>
-				<option value="Dr">Dr</option>
-			</select>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className='form-input'
+        type='text'
+        placeholder='First name'
+        {...register("FirstName", { required: true, maxLength: 80 })}
+        aria-invalid={errors.firstName ? "true" : "false"}
+      />
+      {errors.firstName?.type === "required" && (
+        <p role='alert'>First name is required</p>
+      )}
+      <input
+        className='form-input'
+        type='text'
+        placeholder='Last name'
+        {...register("LastName", { required: true, maxLength: 100 })}
+        aria-invalid={errors.LastName ? "true" : "false"}
+      />
+      {errors.LastName?.type === "required" && (
+        <p role='alert'>Last Name is required</p>
+      )}
+      <input
+        className='form-input'
+        type='text'
+        placeholder='Email'
+        {...register("Email", {
+          required: true,
+          pattern: /^\S+@\S+$/i,
+        })}
+        aria-invalid={errors.Email ? "true" : "false"}
+      />
+      {errors.mail && <p role='alert'>"Email Address is required"</p>}
+      <input
+        className='form-input'
+        type='tel'
+        placeholder='Mobile number'
+        {...register("MobileNumber", {
+          required: true,
+          minLength: 6,
+          maxLength: 12,
+        })}
+        aria-invalid={errors.MobileNumber ? "true" : "false"}
+      />
+      {errors.MobileNumber && (
+        <p role='alert'>"Mobile Number Address is required"</p>
+      )}
+      <select {...register("Title", { required: true })}>
+        <option value='Mr'>Mr</option>
+        <option value='Mrs'>Mrs</option>
+        <option value='Miss'>Miss</option>
+        <option value='Dr'>Dr</option>
+      </select>
 
-			<input
-				{...register("Developer", { required: true })}
-				type="radio"
-				value="Yes"
-				className="form-radio-input"
-			/>
-			<input
-				{...register("Developer", { required: true })}
-				type="radio"
-				value="No"
-				className="form-radio-input"
-			/>
+      <input
+        className='form-input'
+        type='password'
+        placeholder='password'
+        {...register("password", {
+          required: true,
+          minLength: {
+            value: 8,
+            message: "Password must be at least 8 characters",
+          },
+          maxLength: 100,
+          pattern: {
+            value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/,
+            message:
+              "Password must include uppercase, lowercase, number, and special character",
+          },
+        })}
+        aria-invalid={errors.password ? "true" : "false"}
+      />
+      {errors.MobileNumber && <p role='alert'>{errors.password.message}</p>}
+      <input
+        className='form-input'
+        type='password'
+        placeholder='PasswordConfirmation'
+        {...register("PasswordConfirmation", {
+          required: true,
+          maxLength: 100,
+          validate: (value) =>
+            value === password || "The passwords do not match",
+        })}
+      />
 
-			<input type="submit" />
-		</form>
-	);
+      <input type='submit' />
+    </form>
+  );
 }
