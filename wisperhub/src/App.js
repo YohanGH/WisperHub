4ca7ec8b4e158ab740c2185477da0c1ad661749f
Joris <130372686+Lith3@users.noreply.github.com>
2024-05-07 18:57:52 +0200

    Lith3 socket.io (#49)
    * Develop (#31)
    
    * Yohan gh 10 conversation (#14)
    
    * Create two files conversation jsx and css module in to folder conversation
    
    * Added components comversation in to files App.jsx
    
    * Added param props components
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Lith3 5 messagecomposer (#15)
    
    * adding an input and functions that update the input on change and put the message in a setter when we press the send button or the enter key
    
    * add sendMessage State in App.jsx
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    
    * Create two files message jsx and css module in to folder message (#16)
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Fix conv bug (#17)
    
    * fix error in conversation.jsx
    
    * rename some folders
    
    * Socket (#24)
    
    * create socket structure
    
    * create serveur with Express
    
    * suppr some comment
    
    * Yohan gh fix socket (#25)
    
    * Modification index.js, update version react 18
    
    * Modification index.js, update version react 18
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Import frame motion create loading page and progessBar (#23)
    
    * Import frame motion create loading page and progessBar
    
    * Modification name folder
    
    * Modification webpack for css
    
    * Modification name files
    
    * Modification css
    
    * create component MainApp and create logique for loadingpage
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Lith3 fix message (#27)
    
    * change sendMessage string to an array of object and fix Message.js component to display sendMessage
    
    * delete a comment
    
    * change @param string to array
    
    * Create folder dates and component Dates for message watch hours and Date (#28)
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * 7-emoji (#29)
    
    * create a button emoji who display a emoji panel whose when we click on a emote, the emote is add in the input message
    
    * add some css
    
    * remove a console.log
    
    * Fix emoji (#30)
    
    * create a button emoji who display a emoji panel whose when we click on a emote, the emote is add in the input message
    
    * add some css
    
    * remove a console.log
    
    * close the emoji menu when the message is send
    
    * adjuste the size of Emoji menu
    
    * add some emoji
    
    * add line homepage
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * add first step of socket.io communication msg
    
    * add direct communication
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    Co-authored-by: YohanGH <YohanGH@proton.me>

--- wisperhub/src/App.js
+++ wisperhub/src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "./App.css";
 import { useState, useEffect } from "react";
-import LoadingPage from "./components/loading_page/LoadingPage.js"
+import LoadingPage from "./components/loading_page/LoadingPage.js";
 import MainApp from "./components/MainApp.js";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 5555);
-    return () => clearTimeout(timer)
+    return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <>
-    {isLoading ? (
-      <LoadingPage />
-    ) : (
-      <MainApp />
-    )}
-  </>
-  );
+  return <>{isLoading ? <LoadingPage /> : <MainApp />}</>;
 }
 
 export default App;
--- wisperhub/src/components/message_composer/MessageComposer.js
+++ wisperhub/src/components/message_composer/MessageComposer.js
@@ -2,10 +2,10 @@ import React from "react";
 import InputMessage from "./input_message/InputMessage.js";
 import PropTypes from "prop-types";
 import "./messageComposer-style.css";
-export default function MessageComposer({ setSendMessage }) {
+export default function MessageComposer({ sendMessage, setSendMessage }) {
   return (
     <div id="messageComposer">
-      <InputMessage setSendMessage={setSendMessage} />
+      <InputMessage setSendMessage={setSendMessage} sendMessage={sendMessage} />
     </div>
   );
 }
