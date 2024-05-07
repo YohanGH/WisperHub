bc510f1a271f2e7a195ad29d87ec67faa7be6048
Joris <130372686+Lith3@users.noreply.github.com>
2024-05-07 20:29:18 +0200

    Lith3 username (#54)
    * Develop (#31)
    
    * Yohan gh 10 conversation (#14)
    
    * Create two files conversation jsx and css module in to folder conversation
    
    * Added components comversation in to files App.jsx
    
    * Added param props components
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Lith3 5 messagecomposer (#15)
    
    * adding an input and functions that update the input on change and put the message in a setter when we press the send button or the enter key
    
    * add sendMessage State in App.jsx
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    
    * Create two files message jsx and css module in to folder message (#16)
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Fix conv bug (#17)
    
    * fix error in conversation.jsx
    
    * rename some folders
    
    * Socket (#24)
    
    * create socket structure
    
    * create serveur with Express
    
    * suppr some comment
    
    * Yohan gh fix socket (#25)
    
    * Modification index.js, update version react 18
    
    * Modification index.js, update version react 18
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Import frame motion create loading page and progessBar (#23)
    
    * Import frame motion create loading page and progessBar
    
    * Modification name folder
    
    * Modification webpack for css
    
    * Modification name files
    
    * Modification css
    
    * create component MainApp and create logique for loadingpage
    
    ---------
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * Lith3 fix message (#27)
    
    * change sendMessage string to an array of object and fix Message.js component to display sendMessage
    
    * delete a comment
    
    * change @param string to array
    
    * Create folder dates and component Dates for message watch hours and Date (#28)
    
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * 7-emoji (#29)
    
    * create a button emoji who display a emoji panel whose when we click on a emote, the emote is add in the input message
    
    * add some css
    
    * remove a console.log
    
    * Fix emoji (#30)
    
    * create a button emoji who display a emoji panel whose when we click on a emote, the emote is add in the input message
    
    * add some css
    
    * remove a console.log
    
    * close the emoji menu when the message is send
    
    * adjuste the size of Emoji menu
    
    * add some emoji
    
    * add line homepage
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    Co-authored-by: YohanGH <YohanGH@proton.me>
    
    * add logging component for set a user name before join the conversation
    
    ---------
    
    Co-authored-by: YohanGH <151569122+YohanGH@users.noreply.github.com>
    Co-authored-by: YohanGH <YohanGH@proton.me>

--- wisperhub/src/components/MainApp.js
+++ wisperhub/src/components/MainApp.js
@@ -2,17 +2,26 @@ import React from "react";
 import { useState } from "react";
 import Conversation from "./conversation/conversation.js";
 import MessageComposer from "./message_composer/MessageComposer.js";
+import Logging from "./logging/Logging.js";
 
 export default function MainApp() {
   const [sendMessage, setSendMessage] = useState([]);
+  const [user, setUser] = useState("");
 
   return (
-    <div id="chat">
-      <Conversation sendMessage={sendMessage} />
-      <MessageComposer
-        setSendMessage={setSendMessage}
-        sendMessage={sendMessage}
-      />
-    </div>
+    <>
+      {user.length < 3 ? (
+        <Logging setUser={setUser} />
+      ) : (
+        <div id="chat">
+          <Conversation sendMessage={sendMessage} />
+          <MessageComposer
+            setSendMessage={setSendMessage}
+            sendMessage={sendMessage}
+            user={user}
+          />
+        </div>
+      )}
+    </>
   );
 }
