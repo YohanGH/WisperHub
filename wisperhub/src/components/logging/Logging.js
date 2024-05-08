f4595f32b6f4074e47ae461ca72ab95e9267dd7c
joris budin <jorisbudin@gmail.com>
2024-05-08 14:41:23 +0200

    Co-authored-by: Lith3 <jorisbudin@gmail.com>
    improve css for the logging components and add a logo

--- wisperhub/src/components/logging/Logging.js
+++ wisperhub/src/components/logging/Logging.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./logging-style.css";
+import logo from "../../assets/images/logo.png";
 
 export default function Logging({ setUser }) {
   const [inputText, setInputText] = useState("");
 
   const handleClick = () => {
-    if (inputText.length >= 3 && inputText.length < 28) {
+    if (inputText.length >= 3 && inputText.length < 20) {
       setUser(inputText);
     }
   };
   return (
     <div className="logging">
+      <img src={logo} className="wisperLogo" alt="wisperhub logo" />
       <input
         className="loggingInput"
         type="text"
         value={inputText}
-        onChange={(event) => setInputText(event.target.value)}
+        onChange={(event) => {
+          if (event.target.value.length < 20) {
+            setInputText(event.target.value);
+          }
+        }}
         placeholder="Indiquez votre pseudo"
       />
       <button className="joinButton" type="button" onClick={handleClick}>
